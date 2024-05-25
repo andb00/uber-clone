6f5f42028a873d6733dc83c0afd396a8feaaf00d
andrew <andrew@cs470.local>
2024-05-24 23:58:07 -0700

    Initial Commit

---
+++ uber-clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  uber-clone
+//
+//  Created by Andrew Betancourt on 5/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ uber-clone/uber_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  uber_cloneApp.swift
+//  uber-clone
+//
+//  Created by Andrew Betancourt on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct uber_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
