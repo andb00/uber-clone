878fe05f9aa699e0a167155c36be6f9d671d0861
andrew <andrewbetancourt00@gmail.com>
2024-05-27 00:14:14 -0700

    Adjusts window to display route

--- uber-clone/Core/Home/Views/UberMapViewRepresentable.swift
+++ uber-clone/Core/Home/Views/UberMapViewRepresentable.swift
@@ -31,10 +31,12 @@ struct UberMapViewRepresentable: UIViewRepresentable {
         case .searchingForLocation:
             break
         case .locationSelected:
-            if let coordinate = locationViewModel.selectedLocationCoordinate {
+            if let coordinate = locationViewModel.selectedUberLocation?.coordinate{
                 context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
                 context.coordinator.configurePolyline(withDestinationCoordinate: coordinate)
             }
+            break
+        case .polylineAdded:
             break
         }
         
@@ -93,37 +95,18 @@ extension UberMapViewRepresentable {
             anno.coordinate = coordinate
             parent.mapView.addAnnotation(anno)
             parent.mapView.selectAnnotation(anno, animated: true)
-            
-            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
         }
         
         func configurePolyline(withDestinationCoordinate coordinate: CLLocationCoordinate2D) {
             guard let userLocationCoordinate = self.userLocationCoordinate else {return}
-            getDestinationRoute(from: userLocationCoordinate, to: coordinate, completion: { route in
+            parent.locationViewModel.getDestinationRoute(from: userLocationCoordinate, to: coordinate, completion: { route in
                 self.parent.mapView.addOverlay(route.polyline)
-                
+                self.parent.mapState = .polylineAdded
+                let rect = self.parent.mapView.mapRectThatFits(route.polyline.boundingMapRect,
+                                                               edgePadding: .init(top: 64,
+                                                               left: 32, bottom: 500, right: 32))
+                self.parent.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
             })
-        }
-        
-        func getDestinationRoute(from userLocation: CLLocationCoordinate2D,
-                                 to destination: CLLocationCoordinate2D,
-                                 completion: @escaping(MKRoute) -> Void) {
-            
-            let userPlacemark = MKPlacemark(coordinate: userLocation)
-            let destPlacemark = MKPlacemark(coordinate: destination)
-            let request = MKDirections.Request()
-            request.source = MKMapItem(placemark: userPlacemark)
-            request.destination = MKMapItem(placemark: destPlacemark)
-            let directions = MKDirections(request: request)
-            
-            directions.calculate { response, error in
-                if let error = error {
-                    print("DEBUG: Failed to get directions with error \(error.localizedDescription)")
-                    return
-                }
-                guard let route = response?.routes.first else {return}
-                completion(route)
-            }
         }
         
         func clearMapViewAndRecenterOnUserLocation() {
--- uber-clone/Core/Location Search/Views/LocationSearchView.swift
+++ uber-clone/Core/Location Search/Views/LocationSearchView.swift
@@ -54,14 +54,17 @@ struct LocationSearchView: View {
                     ForEach(viewModel.results, id: \.self) { result in
                         LocationSearchResultCell(title: result.title, subtitle: result.subtitle)
                             .onTapGesture {
-                                viewModel
-                                    .selectLocation(result)
-                                mapState = .locationSelected
+                                withAnimation(.spring()){
+                                    viewModel
+                                        .selectLocation(result)
+                                    mapState = .locationSelected
+                                }
                             }
                     }
                 }
             }
         }
+        .background(Color.theme.backgroundColor)
         .background(.white)
     }
 }
