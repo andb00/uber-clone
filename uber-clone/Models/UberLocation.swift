06d5d516189fc6eacc28649bfd647f79a251db85
andrew <andrewbetancourt00@gmail.com>
2024-05-27 00:18:10 -0700

    Added struct to contain coordinate info

---
+++ uber-clone/Models/UberLocation.swift
@@ -0,0 +1,13 @@
+//
+//  UberLocation.swift
+//  uber-clone
+//
+//  Created by Andrew Betancourt on 5/26/24.
+//
+
+import CoreLocation
+
+struct UberLocation {
+    let title: String
+    let coordinate: CLLocationCoordinate2D
+}
